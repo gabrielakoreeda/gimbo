0cff7f98eab6c54049e484709af8c9bd195e024c
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-08-30 10:06:36 -0300

    feat: add reload param to notas get request

--- pages/api/notas.js
+++ pages/api/notas.js
@@ -1,7 +1,12 @@
 let fs = require("fs");
+import getAllNotas from "../../converter/index";
 const filename = "./notas/notas.json";
 
 export default function handler(req, res) {
+  const { reload } = req.query;
+  if (reload) {
+    getAllNotas();
+  }
   try {
     const notas = fs.readFileSync(filename, "utf8");
     res.status(200).json(notas);
