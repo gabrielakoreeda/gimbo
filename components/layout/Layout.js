20bc2cc8333c41da2045e69bab17e8a40f795807
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-08-30 09:27:26 -0300

    feat: show spinning when loading notas

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,22 @@
 import Header from "./Header";
+import { VscLoading } from "react-icons/vsc";
+import { useContext } from "react";
+import NotasContext from "../../store/notas-context";
 
 const Layout = (props) => {
+  const notasCtx = useContext(NotasContext);
+
   return (
-    <main className="bg-indigo-100 h-screen">
+    <main className="bg-indigo-100 h-screen flex flex-col">
       <Header />
-      <div>{props.children}</div>
+      <div className="flex-grow relative">
+        {notasCtx.isLoading && (
+          <div className="h-full bg-gray-700 bg-opacity-60 absolute z-10 inset-0">
+            <VscLoading className="h-full animate-spin font-bold text-8xl m-auto" />
+          </div>
+        )}
+        {props.children}
+      </div>
     </main>
   );
 };
