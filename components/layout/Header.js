6c0fd7de214628a75ce9c6c425617d2c48ea76eb
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-08-30 09:27:10 -0300

    feat: add reload button to Header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,12 +1,22 @@
+import { useContext } from "react";
 import { FaMoneyBillWave } from "react-icons/fa";
+import { RiRefreshLine } from "react-icons/ri";
+import NotasContext from "../../store/notas-context";
 
 const Header = () => {
+  const notasCtx = useContext(NotasContext);
+
+  const refreshNotasHandler = () => {
+    notasCtx.reloadNotas();
+  };
+
   return (
-    <header className="shadow-2xl w-full p-4 bg-black">
+    <header className="shadow-2xl w-full p-4 bg-black flex justify-between">
       <div className="text-green-500 text-2xl flex items-center space-x-2">
         <FaMoneyBillWave className="text-3xl" />
         <p className="font-bold">Gimbo</p>
       </div>
+      <RiRefreshLine className="text-white text-3xl cursor-pointer" />
     </header>
   );
 };
