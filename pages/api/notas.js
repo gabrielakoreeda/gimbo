9bfcb1feb7d9746d7d396a2ff8bef05533f2be11
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-08-30 08:53:13 -0300

    feat: add GET request to fetch notas

---
+++ pages/api/notas.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+const filename = "./notas/notas.jso";
+
+export default function handler(req, res) {
+  try {
+    const notas = fs.readFileSync(filename, "utf8");
+    res.status(200).json(notas);
+  } catch (err) {
+    res.status(500).send(`Error reading ${filename}`);
+  }
+}
