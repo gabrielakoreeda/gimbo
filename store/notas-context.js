131e50df4383a016d8807c1987edc410c37c969a
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-08-30 09:26:31 -0300

    feat: add isLoading to context

--- store/notas-context.js
+++ store/notas-context.js
@@ -1,17 +1,21 @@
-import { useEffect, createContext, useCallback } from "react";
+import { useEffect, createContext, useCallback, useState } from "react";
 
 const NotasContext = createContext({
   notas: [],
+  isLoading: false,
   reloadNotas: () => {},
 });
 
 export const NotasContextProvider = (props) => {
-  const [notas, setNotas] = [];
+  const [notas, setNotas] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const retrieveNotasFile = useCallback(async () => {
+    setIsLoading(true);
     const response = await fetch("http://localhost:3000/api/notas");
     response.json().then((data) => setNotas(data));
-  }, [setNotas]);
+    setIsLoading(false);
+  }, [setNotas, setIsLoading]);
 
   useEffect(() => {
     retrieveNotasFile();
@@ -19,6 +23,7 @@ export const NotasContextProvider = (props) => {
 
   const contextValue = {
     notas: notas,
+    isLoading: isLoading,
     reloadNotas: retrieveNotasFile,
   };
 
@@ -28,3 +33,5 @@ export const NotasContextProvider = (props) => {
     </NotasContext.Provider>
   );
 };
+
+export default NotasContext;
