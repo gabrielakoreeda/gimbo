c3a5db80439f76bada2f19803e4de0c8664deedd
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-08-31 18:45:28 -0300

    feat: add filter and group by when fetching notas

--- pages/api/notas.js
+++ pages/api/notas.js
@@ -1,16 +1,24 @@
 let fs = require("fs");
 import getAllNotas from "../../converter/index";
+import { filterNotas, groupBy } from "../../utils/filter";
 const filename = "./notas/notas.json";
 
 export default function handler(req, res) {
-  const { reload } = req.query;
+  const { reload, startDate, endDate, type, ticker } = req.query;
   if (reload) {
     getAllNotas();
   }
   try {
-    const notas = fs.readFileSync(filename, "utf8");
-    res.status(200).json(notas);
+    const notas = JSON.parse(fs.readFileSync(filename, "utf8"));
+    const filteredNotas = filterNotas(notas, startDate, endDate, type, ticker);
+    if (!ticker) {
+      const groupedNotas = groupBy(filteredNotas);
+      res.status(200).json(groupedNotas);
+    } else {
+      res.status(200).json(filteredNotas);
+    }
   } catch (err) {
+    console.log(err);
     res.status(500).send(`Error reading ${filename}`);
   }
 }
