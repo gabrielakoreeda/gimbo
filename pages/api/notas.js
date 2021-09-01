6406dba35122c81b77e4ea1237a9499416155cac
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-01 10:46:36 -0300

    feat: add PUT method to notas

--- pages/api/notas.js
+++ pages/api/notas.js
@@ -1,24 +1,41 @@
 let fs = require("fs");
 import getAllNotas from "../../converter/index";
-import { filterNotas, groupBy } from "../../utils/filter";
+import { filterNotas, groupBy, editTicker } from "../../utils/filter";
 const filename = "./notas/notas.json";
 
 export default function handler(req, res) {
-  const { reload, startDate, endDate, type, ticker } = req.query;
-  if (reload) {
-    getAllNotas();
-  }
-  try {
-    const notas = JSON.parse(fs.readFileSync(filename, "utf8"));
-    const filteredNotas = filterNotas(notas, startDate, endDate, type, ticker);
-    if (!ticker) {
-      const groupedNotas = groupBy(filteredNotas);
-      res.status(200).json(groupedNotas);
-    } else {
-      res.status(200).json(filteredNotas);
+  if (req.method === "GET") {
+    const { reload, startDate, endDate, type, ticker } = req.query;
+    if (reload) {
+      getAllNotas();
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(`Error reading ${filename}`);
+    try {
+      const notas = JSON.parse(fs.readFileSync(filename, "utf8"));
+      const filteredNotas = filterNotas(
+        notas,
+        startDate,
+        endDate,
+        type,
+        ticker
+      );
+      if (!ticker) {
+        const groupedNotas = groupBy(filteredNotas);
+        res.status(200).json(groupedNotas);
+      } else {
+        res.status(200).json(filteredNotas);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(`Error reading ${filename}`);
+    }
+  } else if (req.method === "PUT") {
+    const { ticker, newTicker, type } = JSON.parse(req.body);
+    try {
+      editTicker(ticker, newTicker, type);
+      res.status(200).send("Successfully updated ticker");
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Error updating ticker");
+    }
   }
 }
