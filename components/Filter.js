a9fa120af540176ac410aadaf0eb2acae21e0704
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-08-31 21:06:24 -0300

    feat: add Filter component

---
+++ components/Filter.js
@@ -0,0 +1,41 @@
+import { useContext, useEffect, useRef } from "react";
+import NotasContext from "../store/notas-context";
+import Button from "./ui/Button";
+
+const Filter = () => {
+  const notasCtx = useContext(NotasContext);
+  const startRef = useRef();
+  const endRef = useRef();
+
+  const filterHandler = () => {
+    notasCtx.filter(startRef.current.value, endRef.current.value);
+  };
+
+  return (
+    <div className="space-x-2 w-full py-5 px-10 flex justify-end items-center">
+      <span className="mr-2 font-bold text-gray-800">
+        Filtrar por período de
+      </span>
+      <input
+        type="month"
+        id="start"
+        name="start"
+        min="2018-03"
+        className="rounded p-1"
+        ref={startRef}
+      />
+      <p className="font-bold text-gray-800">até</p>
+      <input
+        type="month"
+        id="start"
+        name="start"
+        min="2018-03"
+        className="rounded p-1"
+        ref={endRef}
+      />
+      <Button onClick={filterHandler}>Buscar</Button>
+    </div>
+  );
+};
+
+export default Filter;
