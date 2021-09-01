a171f2b6dfec06489d5d69ef6c3a4c61fe740408
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-01 06:43:14 -0300

    fix: add vertical scroll when needed

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -9,7 +9,7 @@ const Layout = (props) => {
   return (
     <main className="bg-gray-200 h-screen flex flex-col">
       <Header />
-      <div className="flex-grow relative px-10 py-5">
+      <div className="flex-grow relative px-10 py-5 overflow-y-auto">
         {notasCtx.isLoading && (
           <div className="h-full bg-gray-700 bg-opacity-60 absolute z-10 inset-0">
             <VscLoading className="h-full animate-spin font-bold text-8xl m-auto" />
