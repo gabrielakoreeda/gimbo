fc766014bdac6d182a069d0cf991eac38c153c67
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-01 10:53:53 -0300

    feat: add basic components to home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-import Layout from "../components/layout/Layout";
+import Filter from "../components/Filter";
+import TablePosicoes from "../components/table/TablePosicoes";
+import NotasContext from "../store/notas-context";
+import { useContext } from "react";
 
 export default function Home() {
-  return <Layout></Layout>;
+  const notasCtx = useContext(NotasContext);
+  const notas = notasCtx.notas;
+
+  return (
+    <>
+      <Filter />
+      <div className="flex flex-col">
+        <h2 className="font-bold text-gray-800 mb-2">Posições Abertas</h2>
+        <TablePosicoes data={notas} />
+      </div>
+    </>
+  );
 }
