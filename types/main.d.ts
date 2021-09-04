b79e475b77d8e82d74d4ccd54aaab85a6806271b
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-04 12:53:48 -0300

    feat: add optional fields to Nota type

--- types/main.d.ts
+++ types/main.d.ts
@@ -2,18 +2,18 @@ interface Nota {
   id: number;
   ticker: string;
   tipo: string;
-  Negociação: string;
+  descricao?: string;
+  Negociação?: string;
   "C/V": string;
-  "Tipo mercado": string;
-  "Especificação do título": string;
-  "Obs. (*)": string;
+  "Tipo mercado"?: string;
+  "Especificação do título"?: string;
+  "Obs. (*)"?: string;
   Quantidade: number;
   "Preço / Ajuste": number;
   "Valor Operação / Ajuste": number;
-  "D/C": string;
+  "D/C"?: string;
   Corretora: string;
   "Data pregão": string;
-  type: FII;
 }
 
 interface NotaConsolidada {
