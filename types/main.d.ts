c13d5d881f25bc597dadb592b053da87a3f53778
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-04 10:34:07 -0300

    feat: add Nota types

---
+++ types/main.d.ts
@@ -0,0 +1,17 @@
+interface Nota {
+  id: number;
+  ticker: string;
+  tipo: string;
+  Negociação: string;
+  "C/V": string;
+  "Tipo mercado": string;
+  "Especificação do título": string;
+  "Obs. (*)": string;
+  Quantidade: number;
+  "Preço / Ajuste": number;
+  "Valor Operação / Ajuste": number;
+  "D/C": string;
+  Corretora: string;
+  "Data pregão": string;
+  type: FII;
+}
