d11978536a8fa2a7f6747ca615d4f006afc1c61d
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-04 11:03:07 -0300

    refactor: migrate TableWrapper to typescript

--- components/table/TableWrapper.js
+++
@@ -1,9 +0,0 @@
-const TableWrapper = (props) => {
-  return (
-    <div className="rounded border border-b-0 border-gray-400">
-      {props.children}
-    </div>
-  );
-};
-
-export default TableWrapper;
---
+++ components/table/TableWrapper.tsx
@@ -0,0 +1,7 @@
+const TableWrapper: React.FC = ({ children }) => {
+  return (
+    <div className="rounded border border-b-0 border-gray-400">{children}</div>
+  );
+};
+
+export default TableWrapper;
