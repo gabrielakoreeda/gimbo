9930300cfb3e26716b6974db5e5d125478b0baf5
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-04 11:28:03 -0300

    refactor: migrate Home page to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Filter from "@components/Filter";
 import TablePosicoes from "@components/table/TablePosicoes";
 import NotasContext from "@store/notas-context";
+import { NextPage } from "next";
 import { useContext } from "react";
 
-export default function Home() {
+const Home: NextPage = () => {
   const notasCtx = useContext(NotasContext);
   const notas = notasCtx.notas;
 
@@ -12,8 +13,10 @@ export default function Home() {
       <Filter />
       <div className="flex flex-col">
         <h2 className="font-bold text-gray-800 mb-2">Posições Abertas</h2>
-        <TablePosicoes data={notas} />
+        <TablePosicoes notas={notas} />
       </div>
     </>
   );
-}
+};
+
+export default Home;
