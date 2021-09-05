46ea7d19eff4eee9c9ef7e91b6c99de0a0660600
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-05 09:43:16 -0300

    refactor: add filter and groupBy logic to context

--- store/notas-context.tsx
+++ store/notas-context.tsx
@@ -1,28 +1,46 @@
+import { groupBy } from "@utils/query-notas";
 import { useRouter } from "next/dist/client/router";
 import { useEffect, createContext, useCallback, useState } from "react";
 
 const endpoint = "http://localhost:3000/api/notas?";
 
 interface NotasContextType {
-  notas: NotaConsolidada[];
+  notasConsolidadas: NotaConsolidada[];
+  currentTicker: Nota[];
   isLoading: boolean;
   reloadNotas: (reload: boolean) => void;
   filter: (start: string, end: string) => void;
-  getTicker: (ticker: string) => Promise<Nota[]>;
+  getTicker: (ticker: string) => void;
   editTicker: (ticker: string, newTicker: string, type: string) => void;
+  addNewOperation: (operation: {
+    ticker: string;
+    operationType: string;
+    quantity: number;
+    price: number;
+    priceTotal: number;
+    date: string;
+    corretora: string;
+    description: string;
+  }) => void;
 }
 
 const NotasContext = createContext<NotasContextType>({
-  notas: [],
+  notasConsolidadas: [],
+  currentTicker: [],
   isLoading: false,
   reloadNotas: () => {},
   filter: () => {},
-  getTicker: async () => [],
+  getTicker: () => [],
   editTicker: () => {},
+  addNewOperation: () => {},
 });
 
 export const NotasContextProvider: React.FC = ({ children }) => {
-  const [notas, setNotas] = useState([]);
+  const [notas, setNotas] = useState<Nota[]>([]);
+  const [notasConsolidadas, setNotasConsolidadas] = useState<NotaConsolidada[]>(
+    []
+  );
+  const [currentTicker, setCurrentTicker] = useState<Nota[]>();
   const [filterPeriod, setFilterPeriod] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
@@ -38,27 +56,24 @@ export const NotasContextProvider: React.FC = ({ children }) => {
       if (reload) {
         params.append("reload", "true");
       }
-      if (filterPeriod.length === 2) {
-        params.append("startDate", filterPeriod[0]);
-        params.append("endDate", filterPeriod[1]);
-      }
       const response = await fetch(endpoint + params.toString());
       response.json().then((data) => {
         setNotas(data);
       });
       setIsLoading(false);
     },
-    [setNotas, setIsLoading, filterPeriod]
+    [setNotas, setIsLoading]
   );
 
-  const getTicker = useCallback(async (ticker) => {
-    setIsLoading(true);
-    const params = { ticker };
-    const response = await fetch(endpoint + new URLSearchParams(params));
-    const nota = await response.json();
-    setIsLoading(false);
-    return nota;
-  }, []);
+  const getTicker = useCallback(
+    (ticker) => {
+      setIsLoading(true);
+      const filteredNotas = notas.filter((nota) => nota.ticker === ticker);
+      setCurrentTicker(filteredNotas);
+      setIsLoading(false);
+    },
+    [notas]
+  );
 
   const editTicker = (ticker, newTicker, type) => {
     setIsLoading(true);
@@ -73,17 +88,36 @@ export const NotasContextProvider: React.FC = ({ children }) => {
     setIsLoading(false);
   };
 
+  const addNewOperation = (operation) => {
+    setIsLoading(true);
+    fetch(endpoint, {
+      method: "POST",
+      body: JSON.stringify(operation),
+    }).then(async (response) => {
+      const newOperation = await response.json();
+      setNotas((prev) => [...prev, newOperation]);
+    });
+    setIsLoading(false);
+  };
+
   useEffect(() => {
     retrieveNotasFile(false);
   }, [retrieveNotasFile]);
 
+  useEffect(() => {
+    const notasConsolidadas = groupBy(notas);
+    setNotasConsolidadas(notasConsolidadas);
+  }, [notas]);
+
   const contextValue = {
-    notas: notas,
+    notasConsolidadas: notasConsolidadas,
+    currentTicker: currentTicker,
     isLoading: isLoading,
     reloadNotas: retrieveNotasFile,
     filter: filterHandler,
     getTicker: getTicker,
     editTicker: editTicker,
+    addNewOperation: addNewOperation,
   };
 
   return (
