e7b7cc593e4cbb8e8f04da498899c9092c819a2d
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-05 17:18:54 -0300

    feat: only show original notas in TableAtivo

--- components/table/TableAtivo.tsx
+++ components/table/TableAtivo.tsx
@@ -21,20 +21,22 @@ const TableAtivo: React.FC<{ notas: Nota[] }> = ({ notas }) => {
         </thead>
         <tbody>
           {notas.map((nota) => {
-            return (
-              <tr key={nota.id}>
-                <td>{nota["Negociação"]}</td>
-                <td>{nota["C/V"]}</td>
-                <td>{nota["Tipo mercado"]}</td>
-                <td>{nota["Especificação do título"]}</td>
-                <td>{nota["Quantidade"]}</td>
-                <td>{formatMoney(nota["Preço / Ajuste"])}</td>
-                <td>{formatMoney(nota["Valor Operação / Ajuste"])}</td>
-                <td>{nota["D/C"]}</td>
-                <td>{nota["Corretora"]}</td>
-                <td>{nota["Data pregão"]}</td>
-              </tr>
-            );
+            if (!nota.manual) {
+              return (
+                <tr key={nota.id}>
+                  <td>{nota["Negociação"]}</td>
+                  <td>{nota["C/V"]}</td>
+                  <td>{nota["Tipo mercado"]}</td>
+                  <td>{nota["Especificação do título"]}</td>
+                  <td>{nota["Quantidade"]}</td>
+                  <td>{formatMoney(nota["Preço / Ajuste"])}</td>
+                  <td>{formatMoney(nota["Valor Operação / Ajuste"])}</td>
+                  <td>{nota["D/C"]}</td>
+                  <td>{nota["Corretora"]}</td>
+                  <td>{nota["Data pregão"]}</td>
+                </tr>
+              );
+            }
           })}
         </tbody>
       </table>
