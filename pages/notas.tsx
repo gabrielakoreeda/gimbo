954f8f8c021f594ba05d576e1859cf680445e356
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 19:52:22 -0300

    feat: add TableAtivo to notas page

--- pages/notas.tsx
+++ pages/notas.tsx
@@ -1,11 +1,18 @@
+import TableAtivo from "@components/table/TableAtivo";
 import PageTitle from "@components/ui/PageTitle";
+import NotasContext from "@store/notas-context";
 import { NextPage } from "next";
+import { useContext } from "react";
 
 const Notas: NextPage = () => {
+  const notaCtx = useContext(NotasContext);
+  const notas = notaCtx.notas;
+
   return (
-    <>
+    <div className="flex flex-col h-full">
       <PageTitle title="Notas" />
-    </>
+      <TableAtivo notas={notas} />
+    </div>
   );
 };
 
