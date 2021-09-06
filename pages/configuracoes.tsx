80a9a5aca2641252570964972905ff3dc6df6fd9
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 16:19:43 -0300

    feat: add empty Configuracoes page

---
+++ pages/configuracoes.tsx
@@ -0,0 +1,12 @@
+import PageTitle from "@components/ui/PageTitle";
+import { NextPage } from "next";
+
+const Configuracoes: NextPage = () => {
+  return (
+    <>
+      <PageTitle title="Configurações" />
+    </>
+  );
+};
+
+export default Configuracoes;
