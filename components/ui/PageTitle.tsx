81b40259b372a9ac2a7b7e41fc10c7075824d527
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 16:15:20 -0300

    fix: add margin bottom to PageTitle component

--- components/ui/PageTitle.tsx
+++ components/ui/PageTitle.tsx
@@ -1,5 +1,7 @@
 const PageTitle: React.FC<{ title: string }> = (props) => {
-  return <h1 className="font-bold text-grey-800 text-xl">{props.title}</h1>;
+  return (
+    <h1 className="font-bold text-grey-800 text-xl mb-4">{props.title}</h1>
+  );
 };
 
 export default PageTitle;
