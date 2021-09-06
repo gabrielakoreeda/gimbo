f644d0f4481533b525b8fe1e88018081e9c309a7
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 16:28:06 -0300

    fix: fix typo in text-gray

--- components/ui/PageTitle.tsx
+++ components/ui/PageTitle.tsx
@@ -1,6 +1,6 @@
 const PageTitle: React.FC<{ title: string }> = (props) => {
   return (
-    <h1 className="font-bold text-grey-800 text-xl mb-4">{props.title}</h1>
+    <h1 className="font-bold text-gray-800 text-xl mb-4">{props.title}</h1>
   );
 };
 
