2b37c99ea14a053daab7e70327eff9573970f090
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 16:06:17 -0300

    feat: replace Header with Sidebar in Layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 import { VscLoading } from "react-icons/vsc";
 import { useContext } from "react";
 import NotasContext from "@store/notas-context";
@@ -7,8 +8,9 @@ const Layout: React.FC = ({ children }) => {
   const notasCtx = useContext(NotasContext);
 
   return (
-    <main className="bg-gray-200 h-screen flex flex-col">
-      <Header />
+    <main className="bg-gray-100 h-screen flex">
+      {/* <Header /> */}
+      <Sidebar />
       <div className="flex-grow relative px-10 py-5 overflow-y-hidden">
         {notasCtx.isLoading && (
           <div className="h-full bg-gray-700 bg-opacity-60 absolute z-10 inset-0">
