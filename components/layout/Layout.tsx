980da7c14a525d68545d7d70126bacabf12e3a61
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 16:58:25 -0300

    fix: add fixed width to content div

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC = ({ children }) => {
     <main className="bg-gray-100 h-screen flex">
       {/* <Header /> */}
       <Sidebar />
-      <div className="flex-grow relative px-10 py-5 overflow-y-hidden">
+      <div className="w-5/6 relative px-10 py-5 overflow-y-hidden">
         {notasCtx.isLoading && (
           <div className="h-full bg-gray-700 bg-opacity-60 absolute z-10 inset-0">
             <VscLoading className="h-full animate-spin font-bold text-8xl m-auto" />
