6f48a748068377c464543923ba7aa48d13e52673
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 16:36:04 -0300

    feat: add content to Configuracoes page

--- pages/configuracoes.tsx
+++ pages/configuracoes.tsx
@@ -1,3 +1,4 @@
+import Button from "@components/ui/Button";
 import PageTitle from "@components/ui/PageTitle";
 import { NextPage } from "next";
 
@@ -5,6 +6,35 @@ const Configuracoes: NextPage = () => {
   return (
     <>
       <PageTitle title="Configurações" />
+      <div>
+        <h2 className="font-bold text-gray-600 text-md">Resetar as notas</h2>
+        <div className="flex justify-between items-center mb-10">
+          <p className="text-gray-600">
+            Lê novamente as notas de negociações e remove as notas
+            personalizadas adicionadas manualmente.
+          </p>
+          <Button>Aplicar</Button>
+        </div>
+        <h2 className="font-bold text-gray-600 text-md">
+          Carregar novas notas
+        </h2>
+        <div className="flex justify-between items-center mb-10">
+          <p className="text-gray-600">
+            Lê novamente as notas de negociações e adiciona as já existentes sem
+            apagar as notas personalizadas adicionadas manualmente.
+          </p>
+          <Button>Aplicar</Button>
+        </div>
+        <h2 className="font-bold text-gray-600 text-md">
+          Remover notas personalizadas
+        </h2>
+        <div className="flex justify-between items-center">
+          <p className="text-gray-600">
+            Remove todas as notas personalizadas adicionadas manualmente.
+          </p>
+          <Button>Aplicar</Button>
+        </div>
+      </div>
     </>
   );
 };
