a67f87a36d2e070afcad3b59fe7237371066b00a
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 16:20:25 -0300

    feat: add empty Ativos page

---
+++ pages/ativos/index.tsx
@@ -0,0 +1,12 @@
+import PageTitle from "@components/ui/PageTitle";
+import { NextPage } from "next";
+
+const Ativos: NextPage = () => {
+  return (
+    <>
+      <PageTitle title="Ativos" />
+    </>
+  );
+};
+
+export default Ativos;
