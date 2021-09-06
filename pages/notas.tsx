264bad8813fb768f5e7a520fcd4573d8185cb9f7
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 16:21:17 -0300

    feat: add empty Notas page

---
+++ pages/notas.tsx
@@ -0,0 +1,12 @@
+import PageTitle from "@components/ui/PageTitle";
+import { NextPage } from "next";
+
+const Notas: NextPage = () => {
+  return (
+    <>
+      <PageTitle title="Notas" />
+    </>
+  );
+};
+
+export default Notas;
