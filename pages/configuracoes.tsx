d05ad28652dff4229cc66abaca6051ec04e6110a
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-06 17:44:16 -0300

    feat: add onClick listeners on buttons

--- pages/configuracoes.tsx
+++ pages/configuracoes.tsx
@@ -1,8 +1,24 @@
 import Button from "@components/ui/Button";
 import PageTitle from "@components/ui/PageTitle";
+import NotasContext from "@store/notas-context";
 import { NextPage } from "next";
+import { useContext } from "react";
 
 const Configuracoes: NextPage = () => {
+  const notasCtx = useContext(NotasContext);
+
+  const resetNotasHandler = () => {
+    notasCtx.reloadNotas("reset");
+  };
+
+  const reloadNotasHandler = () => {
+    notasCtx.reloadNotas("reload");
+  };
+
+  const removeManualHandler = () => {
+    notasCtx.reloadNotas("remove");
+  };
+
   return (
     <>
       <PageTitle title="Configurações" />
@@ -13,7 +29,7 @@ const Configuracoes: NextPage = () => {
             Lê novamente as notas de negociações e remove as notas
             personalizadas adicionadas manualmente.
           </p>
-          <Button>Aplicar</Button>
+          <Button onClick={resetNotasHandler}>Aplicar</Button>
         </div>
         <h2 className="font-bold text-gray-600 text-md">
           Carregar novas notas
@@ -23,7 +39,7 @@ const Configuracoes: NextPage = () => {
             Lê novamente as notas de negociações e adiciona as já existentes sem
             apagar as notas personalizadas adicionadas manualmente.
           </p>
-          <Button>Aplicar</Button>
+          <Button onClick={reloadNotasHandler}>Aplicar</Button>
         </div>
         <h2 className="font-bold text-gray-600 text-md">
           Remover notas personalizadas
@@ -32,7 +48,7 @@ const Configuracoes: NextPage = () => {
           <p className="text-gray-600">
             Remove todas as notas personalizadas adicionadas manualmente.
           </p>
-          <Button>Aplicar</Button>
+          <Button onClick={removeManualHandler}>Aplicar</Button>
         </div>
       </div>
     </>
