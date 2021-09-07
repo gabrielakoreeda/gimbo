7b0fcd1da6c3d8a267c80e778ea6c3cd5e3882b9
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-07 12:30:59 -0300

    feat: add placeholder prop to SearchBox component

--- components/filter/SearchBox.tsx
+++ components/filter/SearchBox.tsx
@@ -1,10 +1,11 @@
 import { FaSearch } from "react-icons/fa";
 
 const SearchBox: React.FC<{
+  placeholder: string;
   searchText: string;
   setSearchText: (searchText: string) => void;
   className?: string;
-}> = ({ searchText, setSearchText, className }) => {
+}> = ({ placeholder, searchText, setSearchText, className }) => {
   return (
     <div
       className={`flex items-center space-x-2 bg-white border border-transparent focus-within:border-green-300 focus-within:ring-1 focus-within:ring-green-300 ${className}`}
@@ -13,7 +14,7 @@ const SearchBox: React.FC<{
       <input
         className="border-0 w-full focus:border-0 focus:ring-0"
         type="text"
-        placeholder="Buscar por título ou corretora"
+        placeholder={placeholder}
         onChange={(e) => setSearchText(e.target.value)}
         value={searchText || ""}
       />
