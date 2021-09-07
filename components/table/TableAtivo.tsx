cb57fbe93ac0ccd5fbd41e5d3cd53d423dfbd09f
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-07 14:00:30 -0300

    feat: add isLoading prop to TableAtivo

--- components/table/TableAtivo.tsx
+++ components/table/TableAtivo.tsx
@@ -1,7 +1,11 @@
 import { formatMoney } from "@utils/index";
+import { VscLoading } from "react-icons/vsc";
 import TableWrapper from "./TableWrapper";
 
-const TableAtivo: React.FC<{ notas: Nota[] }> = ({ notas }) => {
+const TableAtivo: React.FC<{ notas: Nota[]; isLoading: boolean }> = ({
+  notas,
+  isLoading,
+}) => {
   return (
     <TableWrapper>
       <table>
@@ -20,24 +24,32 @@ const TableAtivo: React.FC<{ notas: Nota[] }> = ({ notas }) => {
           </tr>
         </thead>
         <tbody>
-          {notas.map((nota) => {
-            if (!nota.manual) {
-              return (
-                <tr key={nota.id}>
-                  <td>{nota["Negociação"]}</td>
-                  <td>{nota["C/V"]}</td>
-                  <td>{nota["Tipo mercado"]}</td>
-                  <td>{nota["Especificação do título"]}</td>
-                  <td>{nota["Quantidade"]}</td>
-                  <td>{formatMoney(nota["Preço / Ajuste"])}</td>
-                  <td>{formatMoney(nota["Valor Operação / Ajuste"])}</td>
-                  <td>{nota["D/C"]}</td>
-                  <td>{nota["Corretora"]}</td>
-                  <td>{nota["Data pregão"]}</td>
-                </tr>
-              );
-            }
-          })}
+          {isLoading ? (
+            <tr>
+              <td colSpan={10}>
+                <VscLoading className="h-full animate-spin font-bold text-8xl m-auto" />
+              </td>
+            </tr>
+          ) : (
+            notas.map((nota) => {
+              if (!nota.manual) {
+                return (
+                  <tr key={nota.id}>
+                    <td>{nota["Negociação"]}</td>
+                    <td>{nota["C/V"]}</td>
+                    <td>{nota["Tipo mercado"]}</td>
+                    <td>{nota["Especificação do título"]}</td>
+                    <td>{nota["Quantidade"]}</td>
+                    <td>{formatMoney(nota["Preço / Ajuste"])}</td>
+                    <td>{formatMoney(nota["Valor Operação / Ajuste"])}</td>
+                    <td>{nota["D/C"]}</td>
+                    <td>{nota["Corretora"]}</td>
+                    <td>{nota["Data pregão"]}</td>
+                  </tr>
+                );
+              }
+            })
+          )}
         </tbody>
       </table>
     </TableWrapper>
