8f614be309639cc817a77bfa3e04cccb2f47f428
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-07 12:17:52 -0300

    fix: add missing props to SearchBox component

--- components/filter/SearchBox.tsx
+++ components/filter/SearchBox.tsx
@@ -1,5 +1,10 @@
 import { FaSearch } from "react-icons/fa";
-const SearchBox: React.FC<{ className?: string }> = ({ className }) => {
+
+const SearchBox: React.FC<{
+  searchText: string;
+  setSearchText: (searchText: string) => void;
+  className?: string;
+}> = ({ searchText, setSearchText, className }) => {
   return (
     <div
       className={`flex items-center space-x-2 bg-white border border-transparent focus-within:border-green-300 focus-within:ring-1 focus-within:ring-green-300 ${className}`}
@@ -9,6 +14,8 @@ const SearchBox: React.FC<{ className?: string }> = ({ className }) => {
         className="border-0 w-full focus:border-0 focus:ring-0"
         type="text"
         placeholder="Buscar por título ou corretora"
+        onChange={(e) => setSearchText(e.target.value)}
+        value={searchText || ""}
       />
     </div>
   );
