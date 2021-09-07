2581336e8c8e445639ddc0574d9a725a00bf5f3c
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-07 08:20:36 -0300

    feat: add tab selection to notas type in notas page

--- pages/notas.tsx
+++ pages/notas.tsx
@@ -1,15 +1,23 @@
 import TableAtivo from "@components/table/TableAtivo";
+import TableAtivoPersonalizado from "@components/table/TableAtivoPersonalizado";
 import Button from "@components/ui/Button";
 import PageTitle from "@components/ui/PageTitle";
 import NotasContext from "@store/notas-context";
 import { FaDownload } from "react-icons/fa";
 import { NextPage } from "next";
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 
 const Notas: NextPage = () => {
   const notaCtx = useContext(NotasContext);
   const notas = notaCtx.notas;
   const downloadRef = useRef<HTMLAnchorElement>();
+  const [notasType, setNotasType] = useState("importadas");
+
+  const toggleNotasType = () => {
+    setNotasType((prev) =>
+      prev === "importadas" ? "personalizadas" : "importadas"
+    );
+  };
 
   const downloadTableHandler = () => {
     const replacer = (key, value) => (value === null ? "" : value); // specify how you want to handle null values here
@@ -43,7 +51,33 @@ const Notas: NextPage = () => {
           </a>
         </span>
       </div>
-      <TableAtivo notas={notas} />
+      <div className="flex mb-4">
+        <p
+          className={`border-b-2 pb-2 px-2 ${
+            notasType === "importadas"
+              ? "border-green-600 text-green-600"
+              : "border-gray-300 cursor-pointer"
+          }`}
+          onClick={toggleNotasType}
+        >
+          Notas importadas
+        </p>
+        <p
+          className={`border-b-2 pb-2 px-2 ${
+            notasType === "personalizadas"
+              ? "border-green-600 text-green-600"
+              : "border-gray-300 cursor-pointer"
+          }`}
+          onClick={toggleNotasType}
+        >
+          Notas personalizadas
+        </p>
+        <span className="flex-grow border-b-2 border-gray-300"></span>
+      </div>
+      {notasType === "importadas" && <TableAtivo notas={notas} />}
+      {notasType === "personalizadas" && (
+        <TableAtivoPersonalizado notas={notas} />
+      )}
     </div>
   );
 };
