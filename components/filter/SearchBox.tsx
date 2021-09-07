a5994872da1be9a934a1d82087aa3d222f0fe609
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-07 11:48:10 -0300

    feat: change SearchBox style

--- components/filter/SearchBox.tsx
+++ components/filter/SearchBox.tsx
@@ -1,14 +1,14 @@
 import { FaSearch } from "react-icons/fa";
 const SearchBox = () => {
   return (
-    <span className="flex items-center space-x-2 bg-white">
-      <FaSearch className="text-gray-400 ml-2" />
+    <div className="flex items-center space-x-2 bg-white border border-transparent focus-within:border-green-300 focus-within:ring-1 focus-within:ring-green-300">
+      <FaSearch className="text-gray-400 mx-2" />
       <input
-        className="border-0 w-full"
+        className="border-0 w-full focus:border-0 focus:ring-0"
         type="text"
         placeholder="Buscar por título ou corretora"
       />
-    </span>
+    </div>
   );
 };
 
