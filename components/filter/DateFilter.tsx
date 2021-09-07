3cdecafacf23fcc2c7aef7a1aaf2428412e30a85
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-07 11:48:52 -0300

    feat: add date range picker to DateFilter

--- components/filter/DateFilter.tsx
+++ components/filter/DateFilter.tsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
+import { FaCalendar } from "react-icons/fa";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const DateFilter: React.FC = () => {
-  const [startDate, setStartDate] = useState<Date>(null);
-  const [endDate, setEndDate] = useState<Date>(null);
-
+const DateFilter: React.FC<{
+  startDate: Date;
+  endDate: Date;
+  setStartDate: (date: Date) => void;
+  setEndDate: (date: Date) => void;
+}> = ({ startDate, endDate, setStartDate, setEndDate }) => {
   return (
-    <DatePicker
-      selectsRange={true}
-      startDate={startDate}
-      endDate={endDate}
-      onChange={(update) => {
-        setStartDate(update[0]);
-        setEndDate(update[1]);
-      }}
-      isClearable={true}
-    />
+    <div className="flex items-center space-x-2 bg-white border border-transparent focus-within:border-green-300 focus-within:ring-green-300 focus-within:ring-1">
+      <FaCalendar className="text-gray-400 mx-2" />
+      <DatePicker
+        selectsRange={true}
+        startDate={startDate}
+        endDate={endDate}
+        onChange={(update) => {
+          setStartDate(update[0]);
+          setEndDate(update[1]);
+        }}
+        isClearable={true}
+        placeholderText="Data pregão"
+        className="border-0 focus-within:ring-0"
+      />
+    </div>
   );
 };
 
