e84dedf098c43d1bbfff833fe011950cb3ee7dd7
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-08 05:16:04 -0300

    refactor: add EditTickerForm to asset page

--- pages/ativos/[id].tsx
+++ pages/ativos/[id].tsx
@@ -2,9 +2,9 @@ import { useRouter } from "next/dist/client/router";
 import { useContext, useEffect, useState } from "react";
 import TableAtivo from "@components/table/TableAtivo";
 import NewOperationForm from "@components/forms/NewOperationForm";
-import Button from "@components/ui/Button";
 import NotasContext from "@store/notas-context";
 import ErrorMessagePopUp from "@components/ui/ErrorMessagePopUp";
+import EditTickerForm from "@components/forms/EditTickerForm";
 
 const Ativo = () => {
   const notaCtx = useContext(NotasContext);
@@ -14,7 +14,6 @@ const Ativo = () => {
   const { id } = router.query;
   const [ticker, setTicker] = useState("");
   const [type, setType] = useState("");
-  const [edit, setEdit] = useState(false);
   const [errorMessages, setErrorMessages] = useState<string[]>([]);
 
   useEffect(() => {
@@ -23,58 +22,20 @@ const Ativo = () => {
     getTicker(ativoTicker);
   }, [getTicker, id]);
 
-  const changeTickerHandler = (e) => {
-    e.preventDefault();
-    setEdit(false);
+  const changeTickerHandler = (ticker, type) => {
     notaCtx.editTicker(id?.toString(), ticker !== id ? ticker : null, type);
-  };
-
-  const editTickerHandler = (e) => {
-    e.preventDefault();
-    setEdit(true);
   };
 
   return (
     <div className="flex flex-col h-full">
       <div className="flex space-x-5 mb-4 items-end">
-        <form
-          className="flex flex-col max-w-min border border-gray-400 rounded p-3 shadow-md"
-          onSubmit={changeTickerHandler}
-        >
-          <p className="text-gray-8 font-bold mb-2">Editar informações</p>
-          <span className="mb-5">
-            <label htmlFor="ticker">Ticker: </label>
-            <input
-              type="text"
-              id="ticker"
-              name="ticker"
-              value={ticker || ""}
-              onChange={(e) => setTicker(e.target.value)}
-              disabled={!edit}
-            />
-          </span>
-          <span className="mb-2">
-            <label htmlFor="type">Tipo:</label>
-            <select
-              id="type"
-              name="type"
-              value={type || ""}
-              className="w-full"
-              onChange={(e) => setType(e.target.value)}
-              disabled={!edit}
-            >
-              <option value="FII">FII</option>
-              <option value="Ação">Ação</option>
-            </select>
-          </span>
-          <span className="w-full text-center">
-            {edit ? (
-              <Button type="submit">Salvar</Button>
-            ) : (
-              <Button onClick={editTickerHandler}>Editar</Button>
-            )}
-          </span>
-        </form>
+        <EditTickerForm
+          ticker={ticker}
+          setTicker={setTicker}
+          type={type}
+          setType={setType}
+          saveTicker={changeTickerHandler}
+        />
         <NewOperationForm ticker={ticker} setErrorMessages={setErrorMessages} />
       </div>
       <TableAtivo notas={notas} />
