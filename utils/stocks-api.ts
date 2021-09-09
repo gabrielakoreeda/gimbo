388c77d223b461d35476b134bd5d79469677d4e7
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-09 12:16:23 -0300

    feat: add stocks api utils functions

---
+++ utils/stocks-api.ts
@@ -0,0 +1,36 @@
+import { getTicker } from "adapters/stock-api";
+import { readFile } from "./wr-notas";
+let fs = require("fs");
+const folder = "./notas/";
+
+const syncTickers = async () => {
+  const notas = readFile();
+  const tickers = {};
+
+  const getTickers = async (notas, tickers) => {
+    return Promise.all(
+      notas.map(async (nota, index) => {
+        if (tickers[nota["Especificação do título"]]) {
+          notas[index].ticker = tickers[nota["Especificação do título"]].ticker;
+        } else {
+          const ticker = await getTicker(nota["Especificação do título"]);
+          notas[index].ticker = ticker;
+          tickers[nota["Especificação do título"]] = ticker;
+        }
+      })
+    );
+  };
+  getTickers(notas, tickers).then(() => {
+    fs.writeFileSync(
+      `${folder}/notas.json`,
+      JSON.stringify(notas),
+      function (err) {
+        if (err) {
+          return console.log(err);
+        }
+      }
+    );
+  });
+};
+
+export { syncTickers };
