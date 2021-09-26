796b9f129a7d538f3c22b0307f6c04dff318cb48
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-26 19:43:47 -0300

    refactor: update tickers endpoint to fetch data from Alpha Vantage

--- pages/api/tickers.ts
+++ pages/api/tickers.ts
@@ -1,10 +1,15 @@
-import { syncTickers } from "@utils/stocks-api";
+import { getInfo } from "adapters/stock-api";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const { func, keywords, symbol } = req.query;
   if (req.method === "GET") {
     try {
-      syncTickers();
-      res.status(200).send("OK");
+      const result = await getInfo({
+        function: func,
+        keywords,
+        symbol,
+      });
+      res.status(200).json(result);
     } catch (err) {
       console.log(err);
       res.status(500).send("Error updating ticker");
