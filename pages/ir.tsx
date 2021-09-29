b315c38568ffe1b39c53c73985e571f6c34f6705
Gabriela Koreeda <gabriela.koreeda@gmail.com>
2021-09-29 15:03:55 -0300

    feat: add content to ir page

--- pages/ir.tsx
+++ pages/ir.tsx
@@ -1,7 +1,50 @@
+import NotasContext from "@store/notas-context";
 import { NextPage } from "next";
+import { useContext, useEffect, useState } from "react";
+import { groupByMonth } from "@utils/query-notas";
+import TableIR from "@components/table/TableIR";
+import Button from "@components/ui/Button";
 
 const IR: NextPage = () => {
-  return <></>;
+  const notasCtx = useContext(NotasContext);
+  const notas = notasCtx.notas;
+  const [referenceYear, setReferenceYear] = useState<string>();
+  const [notasPorMes, setNotasPorMes] = useState({});
+  const [selectedNotas, setSelectedNotas] = useState({});
+
+  useEffect(() => {
+    setNotasPorMes(groupByMonth(notas));
+  }, [notas]);
+
+  useEffect(() => {
+    const currentYear = new Date().getFullYear();
+    setReferenceYear(currentYear.toString());
+  }, []);
+
+  const selectYearHandler = () => {
+    setSelectedNotas(notasPorMes[referenceYear]);
+  };
+
+  return (
+    <>
+      <div className="space-y-4">
+        <span className="space-x-2 flex items-center">
+          <label htmlFor="referenceYear">Ano de referência:</label>
+          <input
+            id="referenceYear"
+            className="w-1/12"
+            type="number"
+            min="2000"
+            step="1"
+            value={referenceYear || new Date().getFullYear()}
+            onChange={(e) => setReferenceYear(e.target.value)}
+          />
+          <Button onClick={selectYearHandler}>Selecionar</Button>
+        </span>
+        <TableIR notas={selectedNotas} />
+      </div>
+    </>
+  );
 };
 
 export default IR;
